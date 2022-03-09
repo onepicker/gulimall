24bdf82a22e87d7b8f8aa802b97a5cc119d9bd3e
picker <445885852@qq.com>
2022-03-09 23:53:48 +0800

    项目初始结构创建

---
+++ guilimall-member/src/test/java/com/atguigu/guilimall/member/GuilimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.guilimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuilimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
